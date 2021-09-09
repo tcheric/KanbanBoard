0ad4a28965810d2fe30295b82384b81abd3cd379
tcheric <echen4401@gmail.com>
2021-09-09 11:31:45 +1000

    removed nested git file

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
