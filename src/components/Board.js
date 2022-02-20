823ede8cb323c8cf85dfa73c9cac86aa0e6a9fef
tcheric <echen4401@gmail.com>
2022-02-20 22:31:46 +1100

    font update

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,26 +5,24 @@ const Board = ({ tasks, onDelete, onToggle, onBackwards, onForward, darkMode, na
   
   return (
     <div className="float-child">
-      {/* <div className={darkMode ? "dark-mode-boards" : "light-mode-boards"}> */}
-        <div className={`boards ${darkMode ? "dark" : "light"}`}>
-          <h2><u>{name}</u></h2>
-          {tasks.length > 0 ? (
-            tasks.map((task) => (
-              <Task 
-                key={task.id} 
-                task={task} 
-                onBackwards={onBackwards} 
-                onForward={onForward} 
-                onDelete={onDelete} 
-                onToggle={onToggle}
-                darkMode={darkMode}>
-              </Task>
-            ))
-          ) : (
-            'No tasks'
-          )}
-        </div>
-      {/* </div> */}
+      <div className={`boards ${darkMode ? "dark" : "light"}`}>
+        <h2><u>{name}</u></h2>
+        {tasks.length > 0 ? (
+          tasks.map((task) => (
+            <Task 
+              key={task.id} 
+              task={task} 
+              onBackwards={onBackwards} 
+              onForward={onForward} 
+              onDelete={onDelete} 
+              onToggle={onToggle}
+              darkMode={darkMode}>
+            </Task>
+          ))
+        ) : (
+          <span>- No tasks -</span>
+        )}
+      </div>
     </div>
   )
 }
